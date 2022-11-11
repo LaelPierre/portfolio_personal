d0f6152148c935f322c73861bca20ebb1e21282c
Lael Pierre <lael.aichaoui@gmail.com>
2022-11-10 21:52:49 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// sidebar toggle
+// select element
+const openBtn = document.getElementById("nav-btn");
+const closeBtn = document.getElementById("close-btn");
+const sidebar = document.getElementById("sidebar");
+const navbar = document.getElementById('nav')
+
+openBtn.addEventListener("click", function () {
+  console.log(sidebar.classList.add("show-sidebar"));
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+
+// fixed navbar
+window.addEventListener('scroll', function () {
+  if(this.window.pageYOffset > 80) {
+    navbar.classList.add('navbar-fixed')
+  } else {
+    navbar.classList.remove('navbar-fixed')
+  }
+})
